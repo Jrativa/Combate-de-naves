aabc1e4195a4626b6ea32849b7db226f4a349c6a
Jrativa <72578389+Jrativa@users.noreply.github.com>
2020-10-19 18:51:08 -0500

    Add files via upload

---
+++ MovePlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 3f;
+    // Update is called once per frame
+    void Update()
+    { 
+      if (Input.GetKey("w")){
+          transform.position += Vector3.up*speed*Time.deltaTime;
+      } 
+      if (Input.GetKey("a")){
+          transform.position += Vector3.left*speed*Time.deltaTime;
+      } 
+      if (Input.GetKey("s")){
+          transform.position += Vector3.down*speed*Time.deltaTime;
+      }
+      if (Input.GetKey("d")){
+          transform.position += Vector3.right *speed*Time.deltaTime;
+      }
+    }
+}
